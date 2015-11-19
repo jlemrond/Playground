bddad25613bad5660cac7cc1630536e470e04361
Jason Lemrond <jason.lemrond@icloud.com>
2015-11-18 20:02:34 -0500

    Add Switch and Text Field 3 restiricion

--- TextFieldsAndDelegates/ViewController.swift
+++ TextFieldsAndDelegates/ViewController.swift
@@ -13,7 +13,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
   var textField1: UITextField!
   var textField2: UITextField!
   var textField3: UITextField!
+  var textField3Switch: UISwitch!
   var textArray: [UITextField]!
+  
   
   let zipCodeDelegate = ZipCodeTextFieldDelegate()
   let currencyDelegate = CurrencyTextFieldDelegate()
@@ -34,23 +36,36 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     textField1 = UITextField(frame: CGRect(x: 15, y: 30, width: textFieldWidth, height: 30))
     textField2 = UITextField(frame: CGRect(x: 15, y: 65, width: textFieldWidth, height: 30))
-    textField3 = UITextField(frame: CGRect(x: 15, y: 100, width: textFieldWidth, height: 30))
+    textField3 = UITextField(frame: CGRect(x: 15, y: 100, width: textFieldWidth - 60, height: 30))
+    textField3Switch = UISwitch(frame: CGRect(x: textFieldWidth - 50 + 15, y: 100, width: 50, height: 30))
     textArray = [textField1, textField2, textField3]
     
     for field in textArray {
-        field.backgroundColor = textFieldColor
-        field.borderStyle = UITextBorderStyle.RoundedRect
-        self.view.addSubview(field)
+      field.backgroundColor = textFieldColor
+      field.borderStyle = UITextBorderStyle.RoundedRect
+      self.view.addSubview(field)
     }
+    
+    self.view.addSubview(textField3Switch)
     
     // Assign Delegates
     textField1.delegate = zipCodeDelegate
     textField2.delegate = currencyDelegate
+    textField3.delegate = self
     
     // Change Keyboard type
-    // textField1.keyboardType = UIKeyboardType.NumberPad
+    textField1.keyboardType = UIKeyboardType.NumberPad
+    textField2.keyboardType = UIKeyboardType.NumberPad
       
   }  // ViewDidLoad End
+  
+  func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+    if textField3Switch.on {
+      return true
+    } else {
+      return false
+    }
+  }
 
 }
 
